c2a36e62be3fd2e92787f7ba4a579cd502643dc4
M. Lourdes Linares <lordesbarrera88@gmail.com>
2022-01-19 22:08:25 +0100

    Codigo comentado centrosAleatorios.hs

--- centrosAleatorios.hs
+++ centrosAleatorios.hs
@@ -2,11 +2,40 @@ module CentrosAleatorios
     (generaCentros
     ) where 
 
+-------------------------------------------------------------------------------
+-- Descripcion general del modulo
+-- Este modulo tiene por objeto generar los centros aleatorios que inicializan el
+-- algoritmo de k-means
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Modulos auxiliares importados
 import Data.Array
 import Data.List
 import System.Random
 import Distancias 
-import Data.Typeable
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Lista de funciones del modulo
+
+-- Funcion listaMinMax :: [Vector] -> [(Double, Double)]
+-- Recibe por parametros una lista de vectores (correspondiente al dataset)
+-- y retorna una lista de (maximo, minimo) de cada coordenada a partir de los
+-- vectores del dataset. 
+-- El objetivo es utilizarla para generar los centros aleatorios teniendo en cuenta
+-- en que rango se mueven los valores de cada coordenada.
+
+-- Parametros:     
+-- lvectores :: [Vector]                    Lista de vectores del dataset
+-- Resultado:
+-- [(min, max)] :: [(Double, Double)]       Lista de valores maximo y minimo de cada coordenada
+
+-- Funciones auxiliares:
+-- minMaxCoord :: Int -> [Vector] -> (Double, Double)   -- Dada una coordenada y los datos devuelve  
+                                                        -- los valores (minimo, maximo) de la misma
+-- extraeValoresCoord :: Int -> [Vector] -> [Double]    -- Dada una coordenada y los datos devuelve 
+                                                        -- la lista de valores que toma esa coordenada
 
 listaMinMax :: [Vector] -> [(Double, Double)]
 listaMinMax lvectores
@@ -22,12 +51,24 @@ minMaxCoord coord lvectores = (minimum lcoord, maximum lcoord)
 extraeValoresCoord :: Int -> [Vector] -> [Double]
 extraeValoresCoord coord lvectores = map(\v -> v!coord) lvectores
 
------------------------
 
-listaVectorIO :: IO[Double] -> IO Vector
-listaVectorIO xs = do
-    xs' <- xs
-    return  (array (1,length xs') [(y,x) | (x,y) <- (zip xs' [1..length xs'])])
+-- Funcion generaCentros :: Int -> [Vector] -> IO [Vector]
+-- Recibe por parametros el numero de centros y lista de vectores (correspondiente 
+-- al dataset) y retorna una lista de centros aleatorios que inicializan el algoritmo
+-- de k-medias.
+
+-- Parametros:     
+-- k :: Int                                 Numero de centros iniciales
+-- lvectores :: [Vector]                    Lista de vectores del dataSet
+-- Resultado:
+-- [centros] :: IO [Vector]                 Lista de centros iniciales
+
+-- Funciones auxiliares:
+-- listaVectorIO :: IO [Double] -> IO Vector                    -- Transforma una lista de valores en un vector  
+-- generaUnNumero :: Int -> [(Double,Double)] -> IO [Double]    -- Dada una lista de intervalos para cada coordenada
+                                                                -- genera un vector donde cada coordenada se mueva 
+                                                                -- dentro del intervalo correspondiente
+                                                                -- Genera los vectores como listas de valores                                                               -- 
 
 generaCentros :: Int -> [Vector] -> IO [Vector]
 generaCentros k lvectores
@@ -37,6 +78,11 @@ generaCentros k lvectores
         x <- listaVectorIO (generaUnNumero 0 listaMinMax2)
         ys <- (generaCentros (k-1) lvectores)
         return ([x] ++ ys)
+
+listaVectorIO :: IO [Double] -> IO Vector
+listaVectorIO xs = do
+    xs' <- xs
+    return  (array (1,length xs') [(y,x) | (x,y) <- (zip xs' [1..length xs'])])
 
 generaUnNumero :: Int -> [(Double,Double)] -> IO [Double]
 generaUnNumero i xs
