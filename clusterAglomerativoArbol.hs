c3d14e0c2d8056c3e4eb9ceec0bb4581132b99c1
M. Lourdes Linares <lordesbarrera88@gmail.com>
2022-01-21 13:48:20 +0100

    Semicometado 2 clusterAglomerativoArbol.hs

--- clusterAglomerativoArbol.hs
+++ clusterAglomerativoArbol.hs
@@ -4,7 +4,20 @@ module ClusterAglomerativoArbol
     clusteringAglomerativoN
     ) where 
 
+-------------------------------------------------------------------------------
+-- Descripcion general del modulo
+-- Este modulo tiene por objeto implementar el algoritmo de clustering aglomerativo
+-- modelandolo mediante un dendograma.
 
+-- El algoritmo comienza con un cluster por cada punto (vector). En las sucesivas 
+-- iteraciones del algoritmo, se toman los dos clusters mas proximos y se fusionan.
+-- El algoritmo finaliza cuando todos los vectores pertenecen al mismo cluster.
+
+-- Imagen de referencia: https://estrategiastrading.com/wp-content/uploads/2019/07/ejemplo_dendrograma.png
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Modulos auxiliares importados
 import Data.Array
 import Data.List
 import System.Random
@@ -13,45 +26,44 @@ import Data.Tree
 import Distancias
 ---------------------------------
 
--- Codigo para probarlo
--- > d = inicializaClusteringAglomerativoA lv
--- > clusteringAglomerativoAux d
+-------------------------------------------------------------------------------
+-- Definicion de tipos
 
--- type Cluster = [Vector]          
-type IdCluster = [Int]
+-- type Cluster = [Vector]              -- Recogido en Distancias.hs        
+type IdCluster = [Int]                  -- Tipo para identificar a un cluster en el arbol
+-- IdCluster = lista de indices de los clusters de nivel inferior contenidos en ese cluster
+-- Como inicialmente los clusters son los puntos, el identificador tambien indica los vectores 
+-- que agrupa ese cluster.
+-- Permite que se pueda visualizar de manera correcta la formacion del dendograma (visualizar
+-- todos los vectores en cada nivel haria ilegible el arbol)
 
-data Nodo = H IdCluster Cluster | N IdCluster Cluster Nodo Nodo  
-    deriving Eq
-
--- (Identificador del cluster , puntos que pertenecen al cluster)
--- Identificador del cluster = indice de los clusters de nivel inferior contenidos en ese cluster
--- Como inicialmente los clusters son los puntos, el identificador tambien nos dice
--- que vectores agrupa ese cluster hasta ese nivel
-
-type Dendogram = [ Nodo ]   -- conforme se construye van quedando bosques (al principio hay hojas sin emparejar)
-                            -- Hasta el final no se obtiene un arbol asi que hay que generalizar el concepto de arbol
-
-instance Show Nodo where 
+data Arbol = H IdCluster Cluster | N IdCluster Cluster Arbol Arbol  
+    deriving Eq                         -- Tipo "arbol"
+-- Este tipo permite reflejar como se van uniendo los clusters. Redefinimos la funcion show 
+-- para que solo muestre el id de los subclusters (nodos) por legibilidad del arbol. 
+instance Show Arbol where 
     show (H id cluster)= "(H " ++ (show id) ++ ")" 
     show (N id cluster denizq dender) = "(N "++ (show id) ++ " "++ show (denizq) ++ " "++ show (dender) ++ ")"
 
-type Nivel = [([Int], Cluster)]
-
-v1 = listaVector [2.0,0.0]
-v2 = listaVector [10.0,20.0]
-v3 = listaVector [2.0,1.0]
-v4 = listaVector [15.0,7.0]
-
-lv = [v1, v2, v3, v4]
-
--- d:: Dendogram 
--- d =  [ ( N [1,2] [v1, v2] (H [1] [v1]) (H [2] [v2] ) ), 
---             (H [3] [v3] ),
---             (H [4] [v4] ) ] 
-
+type Dendogram = [ Arbol ]              -- Tipo "bosque"
+-- Inicialmente, todos los vectores forman un cluster por si mismos (al principio solo hay hojas sin emparejar).
+-- Conforme se van fusionando los clusters se van formando arboles en paralelo (bosque).
+-- Hasta el final no se obtiene un único arbol como tal.
 --------------------------------
 
--- Funcion que dado un dendrograma obtiene el ultimo estado de los clusters
+-------------------------------------------------------------------------------
+-- Lista de funciones "herramienta" del modulo
+
+-- Funcion listaClustersActuales :: Dendogram -> [(IdCluster, Cluster)]
+--         listaClustersActuales2 :: Dendogram -> [Cluster]
+-- Dado un dendrograma obtiene el ultimo estado de los clusters. Tomar el ultimo estado es, 
+-- en definitiva, tomar las raices de todos los arboles del dendograma. 
+
+-- Parametros:
+-- dendograma :: Dendogram                  Bosque de dependencia entre clusters
+-- Resultado:
+-- [(IdCluster, Cluster)]  / [Cluster]      Lista de clusters formados 
+
 listaClustersActuales :: Dendogram -> [(IdCluster, Cluster)]
 listaClustersActuales [] = []
 listaClustersActuales (arbol: xs) = obtenCluster arbol : listaClustersActuales xs
@@ -64,21 +76,38 @@ listaClustersActuales2 (arbol: xs) = obtenCluster arbol : listaClustersActuales2
     where  obtenCluster (N id cluster izq der) = cluster
            obtenCluster (H id cluster) = cluster
 
--- Funcion que dado un cluster = [puntos] devuelve el nodo del dendograma asociado a ese cluster
-nodoAsociadoACluster :: Dendogram -> Cluster -> Maybe Nodo
-nodoAsociadoACluster [] cluster = Nothing
-nodoAsociadoACluster (nodo: xs) cluster = if (clusterNodo == cluster) 
-                                            then Just nodo 
-                                        else (nodoAsociadoACluster xs cluster)
-    where (idNodo, clusterNodo) = datosClusterFromArbol nodo
 
--- Funcion que dado un arbol obtiene el estado del ultimo cluster
-datosClusterFromArbol :: Nodo -> (IdCluster, Cluster)
+-- Funcion arbolAsociadoACluster :: Dendogram -> Cluster -> Maybe Arbol
+-- Funcion que dado un cluster = [Vector] devuelve el arbol del dendograma asociado a 
+-- ese cluster (arbol que tiene como raiz ese cluster)
+
+-- Parametros:
+-- dendogram :: Dendogram               Bosque de dependencia entre clusters
+-- cluster :: cluster                   Cluster 
+-- Resultado:
+-- arbol :: Maybe Arbol                 Arbol asociado a ese cluster
+
+-- Funciones asociadas:
+-- datosClusterFromArbol :: Arbol -> (IdCluster, Cluster)   
+--                                      Funcion que dado un arbol devuelve su cluster asociado 
+--                                      (Identificador de la raiz, cluster raiz) 
+
+arbolAsociadoACluster :: Dendogram -> Cluster -> Maybe Arbol
+arbolAsociadoACluster [] cluster = Nothing
+arbolAsociadoACluster (arbol: xs) cluster = if (clusterArbol == cluster) 
+                                                then Just arbol 
+                                            else 
+                                                arbolAsociadoACluster xs cluster
+    where (idNodo, clusterArbol) = datosClusterFromArbol arbol
+
+
+datosClusterFromArbol :: Arbol -> (IdCluster, Cluster)
 datosClusterFromArbol (N idCluster cluster izq der) = (idCluster, cluster)
 datosClusterFromArbol (H idCluster cluster) = (idCluster, cluster)
 
 
--------------------------------
+-------------------------------------------------------------------------------
+-- Lista de funciones "del algoritmo" del modulo
 
 -- Obtiene el primer nivel a partir de los datos:
 -- todos los elementos forman un cluster por si mismos
@@ -86,7 +115,7 @@ inicializaClusteringAglomerativoA :: [Vector] -> Dendogram
 inicializaClusteringAglomerativoA puntosIniciales = [ (H [indice] [punto] ) | (indice, punto) <- zip [0..] puntosIniciales ]
 
 -- Funcion previa al algoritmo de clustering, elige la forma de representacion de los cluster en la consola
---clusteringAglomerativo :: Dendogram -> String -> Nodo
+--clusteringAglomerativo :: Dendogram -> String -> Arbol
 clusteringAglomerativoA fdistancia dendogram modo
     | modo == "AI" = toDataTreeId (clusteringAglomerativoAux fdistancia dendogram)
     | modo == "AC" = toDataTreeCl (clusteringAglomerativoAux fdistancia dendogram)
@@ -95,7 +124,7 @@ clusteringAglomerativoA fdistancia dendogram modo
 clusteringAglomerativoN fdistancia dendogram = clusteringAglomerativoAux fdistancia dendogram
 
 -- Funcion base del algoritmo de clustering: obtiene la evolucion de la lista de clusters
-clusteringAglomerativoAux :: Distancia -> Dendogram -> Nodo
+clusteringAglomerativoAux :: Distancia -> Dendogram -> Arbol
 clusteringAglomerativoAux fdistancia dendogram 
     | condParada        = head $ dendogram
     | otherwise         = clusteringAglomerativoAux fdistancia (calculaSiguienteNivel fdistancia dendogram)
@@ -112,7 +141,7 @@ calculaSiguienteNivel fdistancia dendogram = newDendogram
             fusionClustersMasCercanos = (N newClusterId newClusterList c1 c2)
             newDendogram = fusionClustersMasCercanos : (eliminaCluster c2 (eliminaCluster c1 dendogram))
         
-eliminaCluster :: Nodo -> Dendogram -> Dendogram
+eliminaCluster :: Arbol -> Dendogram -> Dendogram
 eliminaCluster _ [] = []
 eliminaCluster x (y:ys) 
     | x == y    = ys
@@ -120,8 +149,8 @@ eliminaCluster x (y:ys)
 
 -- Dada una lista de clusters, devuelve el par ( los 2 clusters mas cercanos, distancia entre ellos)
 -- Para medir la distancia entre clusters utiliza la media
-clustersDistanciaMinima :: Distancia -> Dendogram -> ((Nodo, Nodo), Double)
-clustersDistanciaMinima fdistancia d = ((fromJust (nodoAsociadoACluster d c1list), fromJust(nodoAsociadoACluster d c2list)), distancia)
+clustersDistanciaMinima :: Distancia -> Dendogram -> ((Arbol, Arbol), Double)
+clustersDistanciaMinima fdistancia d = ((fromJust (arbolAsociadoACluster d c1list), fromJust(arbolAsociadoACluster d c2list)), distancia)
     where   vss = listaClustersActuales2 d 
             ((c1list, c2list), distancia ) = head(sortBy sndTuple (calculaMatrixProximidad fdistancia vss))
 
@@ -168,3 +197,14 @@ toDataTreeCl (N id cl n1 n2 ) = Node (show (cl)) [toDataTreeCl n1, toDataTreeCl 
 -- d = clusteringAglomerativo distEuclidea a "AI"
 -- e = toDataTree d
 -- f = putStrLn $ drawTree e
+
+-------------------------------------------------------------------------------
+-- Codigo "de juguete" para pruebas unitarias
+-- v1 = listaVector [2.0,0.0]
+-- v2 = listaVector [10.0,20.0]
+-- v3 = listaVector [2.0,1.0]
+-- v4 = listaVector [15.0,7.0]
+-- lv = [v1, v2, v3, v4]
+-- 
+-- 
+-------------------------------------------------------------------------------
